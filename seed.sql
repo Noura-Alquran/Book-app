f8ab2a7970f30d12693a3fdbfceef1baad6663d8
Noura-Alquran <nmalquran15@eng.just.edu.jo>
2021-04-05 20:49:15 +0300

    lab 12 first push

---
+++ seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO books (title, author, isbn, image, description,bookshelf) 
+VALUES('Cooking-Book','Noura & Doha','175','https://i.imgur.com/J5LVHEL.jpg','this book contains a lot of food recipes','Cooking');
--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 'use strict';
+require('dotenv').config();
 const express = require('express');
 const superagent = require('superagent');
 // const path= require('path');
 const app = express();
+const pg=require('pg');
+
 const PORT = process.env.PORT || 5000;
+const DATABASE_URL= process.env.DATABASE_URL;
 app.use(express.static('./public'));
 // app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended:true,}));
+
+// const client = new pg.Client({
+//   connectionString: DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+const client = new pg.Client(process.env.DATABASE_URL);
+
+client.on('error', error => console.error(error));
+
 app.set('view engine', 'ejs');
 
 app.get('/', renderHomePage);
-
 function renderHomePage(req, res) {
-    res.render('pages/index');
+  const booksQuery = 'SELECT * FROM books;';
+  client.query(booksQuery).then((results => {
+    res.render('pages/index', { resultsNum :results.rows.length , results: results.rows });
+  })).catch(error => {
+    res.status(500).render('pages/error',{error :error, massage:'Oops..!Something went wrong'})
+  });
 }
   
 app.get('/searches/new', showForm);
@@ -20,8 +40,6 @@ function showForm(req, res) {
 res.render('pages/searches/new.ejs');
 }
 app.post('/searches', createSearch);
-
-
 function createSearch(req, res) {
     const url = 'https://www.googleapis.com/books/v1/volumes';
     const searchBy = req.body.searchBy;
@@ -38,9 +56,23 @@ function createSearch(req, res) {
     }).then(results => {
       res.render('pages/show', { searchResults: results })
     }).catch((error)=>{
-        res.status(404).render('pages/error',{error :error, massage:'Oops..!Something went wrong'})
+        res.status(500).render('pages/error',{error :error, massage:'Oops..!Something went wrong'})
     })
   }
+  
+  app.get('/books/:id',getSingleBook);
+
+  function getSingleBook(req, res){
+    const bookId = req.params.id;
+    console.log(bookId);
+    const sqlSelectQuery = 'SELECT * FROM books WHERE id=$1';
+    const safeValues = [bookId];
+    client.query(sqlSelectQuery, safeValues).then(results => {
+      res.render('pages/books/detail', { results: results.rows });
+    }).catch((error)=>{
+      res.status(500).render('pages/error',{error :error, massage:'Oops..!Something went wrong'})
+  })
+}
 
 function Books(book) {
     const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
@@ -55,4 +87,9 @@ app.use('*' , function(req , res){
     res.status(404).send('noting to show here');
   });
   
-app.listen(PORT, () => console.log(`Listening to Port ${PORT}`));
+  client.connect().then(() => {
+    app.listen(PORT, () => {
+      console.log("Connected to database:", client.connectionParameters.database) //show what database we connected to
+      console.log(`Listening to Port ${PORT}`);//start point for the application"initialisation"
+    });
+  })
